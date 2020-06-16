00f3a6d58232f6ad9ef83d6b78e5be868ff492ab
shuto masuda <shuto67cow@gmail.com>
2020-06-16 13:11:12 +0900

    kadai2

---
+++ AutoEncoder/affine.py
@@ -0,0 +1,39 @@
+def affine(x, W, b):
+    #　x * W +b -> y
+    y = inner( x, w)
+    y = plus ( y, b)
+    return y
+
+def inner(x,y):
+    n = len(x)
+    m = len(y)
+    l = len(y[0])
+    if len(x[0]) != m:
+        print("Error: inner size")
+        break
+    else:
+        #xのサイズが n*m になっていると想定
+        #yのサイズは　m*l
+        z = [[0 for _ in range(l)] for _ in range(n)]
+        #アクセス順を意識して高速化
+        for i in range(n):
+            for j in range(m):
+                for k in range(l):
+                    z[i][k] += x[i][j]* y[j][k]
+    return z
+
+def plus(x,y):
+    rowx = len(x)
+    rowy = len(y)
+    colx = len(x[0])
+    coly = len(y[0])
+    if rowx != rowy:
+        print("Error: not match matrix row")
+    elif colx != coly:
+        print("Error: not match matrix column")
+    else:
+        for i in range(rowx):
+            for j in range(colx):
+                x[i][j] += y[i][j]
+    
+    return x
